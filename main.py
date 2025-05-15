21650eb01d103ea0ba9a5d50e614b02d52930b46
Cleverson Nahum <cleversonahum@gmail.com>
2025-05-15 16:35:47 -0300

    feat: Implementing Maximum Ratio Transmission and writing external files with SE values

--- main.py
+++ main.py
@@ -5,59 +5,48 @@ os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
 
 import matplotlib.pyplot as plt
 import numpy as np
-import time
 import tensorflow as tf
-
 import sionna
-from sionna.phy import Block
+import csv
 from sionna.phy.mimo import StreamManagement
 from sionna.phy.ofdm import (
     ResourceGrid,
-    ResourceGridMapper,
-    LSChannelEstimator,
-    LMMSEEqualizer,
-    OFDMModulator,
-    OFDMDemodulator,
-    RZFPrecoder,
-    RemoveNulledSubcarriers,
 )
-from sionna.phy.channel.tr38901 import AntennaArray, UMa, PanelArray, CDL
+from sionna.phy.channel.tr38901 import UMa, PanelArray
 from sionna.phy.channel import (
     subcarrier_frequencies,
     cir_to_ofdm_channel,
-    cir_to_time_channel,
-    time_lag_discrete_time_channel,
-    ApplyOFDMChannel,
-    ApplyTimeChannel,
-    OFDMChannel,
-    TimeChannel,
 )
 from sionna.phy.channel import gen_single_sector_topology
-from sionna.phy.fec.ldpc import LDPC5GEncoder, LDPC5GDecoder
-from sionna.phy.mapping import Mapper, Demapper, BinarySource
-from sionna.phy.utils import ebnodb2no, sim_ber, compute_ber
 
-
-num_ut = 2
+sionna.phy.config.seed = 40
+num_ut = 10
 num_bs = 1
+num_bs_ant = 16  # Must be a perfect square
 num_ut_ant = 1
-RB_number = 132
+number_rbs = 132
 subcarrier_spacing = 120e3
 carrier_frequency = 28e9  # Carrier frequency in Hz.
 num_ofdm_symbols = 14
 subcarriers_per_rb = 12
-
+precoding_technique = (
+    "mrt"  # "mrt" = Maximum Ratio Transmission and "none" = equal power allocation
+)
+p_tx = 1.0  # Watts
+n0 = 1e-9  # Noise power spectral density
+batch_size = 10
+time_steps_per_batch = 2
+ues_velocity = 5  # m/s
 num_streams_per_tx = num_ut_ant
-
-
-rx_tx_association = np.array([[1, 1]])
+rx_tx_association = np.ones((num_bs, num_ut))
+plot_ue_se = False  # Plot UE SE per RB for the first batch and num_time_steps element
+write_to_file = True  # Write the results to a file
 
 sm = StreamManagement(rx_tx_association, num_streams_per_tx)
 
 rg = ResourceGrid(
     num_ofdm_symbols=num_ofdm_symbols,
-    fft_size=subcarriers_per_rb
-    * RB_number,  # TODO check if FFT size must obbey to 3GPP
+    fft_size=subcarriers_per_rb * number_rbs,
     subcarrier_spacing=subcarrier_spacing,
     num_tx=num_bs,
     num_streams_per_tx=num_streams_per_tx,
@@ -70,10 +59,10 @@ rg = ResourceGrid(
 
 
 bs_array = PanelArray(
-    num_rows_per_panel=4,
-    num_cols_per_panel=4,
-    polarization="dual",
-    polarization_type="cross",
+    num_rows_per_panel=int(np.sqrt(num_bs_ant)),
+    num_cols_per_panel=int(np.sqrt(num_bs_ant)),
+    polarization="single",
+    polarization_type="V",
     antenna_pattern="38.901",
     carrier_frequency=carrier_frequency,
 )
@@ -97,17 +86,74 @@ channel_model = UMa(
 
 # Set topology
 topology = gen_single_sector_topology(
-    batch_size=1000, num_ut=num_ut, scenario="uma", min_ut_velocity=5, max_ut_velocity=5
+    batch_size=batch_size,
+    num_ut=num_ut,
+    scenario="uma",
+    min_ut_velocity=ues_velocity,
+    max_ut_velocity=ues_velocity,
+    indoor_probability=0.0,
 )
 channel_model.set_topology(*topology)
-channel_model.show_topology(batch_index=0)
-channel_model.show_topology(batch_index=999)
 
-a, tau, *_ = channel_model(num_time_samples=2, sampling_frequency=2e3)
+a, tau, *_ = channel_model(
+    num_time_samples=time_steps_per_batch,
+    sampling_frequency=1 / rg.ofdm_symbol_duration,
+)
 frequencies = subcarrier_frequencies(
-    subcarriers_per_rb * RB_number, rg.subcarrier_spacing
+    subcarriers_per_rb * number_rbs, rg.subcarrier_spacing
 )
 h_freq = cir_to_ofdm_channel(frequencies, a, tau, normalize=True)
 
-plt.show()
-print("")
+
+if precoding_technique == "mrt":  # Maximal Ratio Transmission
+    channel_norms = tf.sqrt(tf.reduce_sum(tf.pow(tf.abs(h_freq), 2), 4))
+    snr = (
+        tf.pow(channel_norms, 2)
+        * p_tx
+        / (n0 * subcarriers_per_rb * number_rbs * rg.subcarrier_spacing)
+    )
+elif precoding_technique == "none":  # Equal power allocation
+    snr = tf.reduce_sum(
+        tf.pow(tf.abs(h_freq), 2)
+        * (p_tx / num_bs_ant)
+        / (n0 * subcarriers_per_rb * number_rbs * rg.subcarrier_spacing),
+        4,
+    )
+else:
+    raise ValueError("Invalid precoding technique. Choose either 'mrt' or 'none'.")
+avg_snr_rb = tf.reduce_mean(
+    tf.reshape(
+        snr,
+        (
+            batch_size,
+            num_ut,
+            num_ut_ant,
+            num_bs,
+            time_steps_per_batch,
+            number_rbs,
+            subcarriers_per_rb,
+        ),
+    ),
+    axis=-1,
+)
+avg_se_rb = np.log2(1 + avg_snr_rb)
+
+if plot_ue_se:  # Plot UE SE per RB for the first batch and num_time_steps element
+    plt.figure()
+    for ue in range(num_ut):
+        plt.plot(avg_se_rb[0, ue, 0, 0, 0, :], label=f"UE {ue+1}")
+    plt.grid()
+    plt.legend()
+    plt.show()
+    print(
+        f"Average achievable throughput per UE (Mbps): {np.mean(avg_se_rb[0, :, 0, 0, 0, :], axis=-1) * subcarriers_per_rb * number_rbs * subcarrier_spacing / 1e6}"
+    )
+if write_to_file:  # Write the results to a file
+    for ue in range(num_ut):
+        with open(f"channel_files/ue_{ue+1}_se.csv", "w") as file:
+            for batch_idx in range(batch_size):
+                for time_step in range(time_steps_per_batch):
+                    writer = csv.writer(file)
+                    writer.writerow(
+                        avg_se_rb[batch_idx, ue, 0, 0, time_step, :]
+                    )  # Write the entire list as a single row
